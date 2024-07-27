a60922b6f595206bf2ae5d5a22933880c2ca9f94
ctl <133525672+centretruckingllc@users.noreply.github.com>
2024-07-27 15:03:55 -0400

    Update generate_index.py

--- generate_index.py
+++ generate_index.py
@@ -11,7 +11,7 @@ def generate_index():
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Video Gallery</title>
+    <title>Animals</title>
     <style>
         body {
             margin: 0;
